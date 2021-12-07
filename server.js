86bfe401d2ba5555590e0e1aa326a9be22ddd17d
MUFF Fabian <fabian.muff@unifr.ch>
2021-12-07 02:43:30 -0800

    certificate for diufvm08 and multiserver

--- server.js
+++ server.js
@@ -1,15 +1,55 @@
 const express = require("express");
 const { Server } = require("socket.io");
 
-const port = process.env.PORT || 3000;
+const fs = require('fs');
+const key = fs.readFileSync('./key.pem');
+const cert = fs.readFileSync('./diufvm08_unifr_ch.pem');
+const https = require('https');
 
-const app = express();
+const port = process.env.PORT || 3001;
+
+
+const app1 = express();
+const app2 = express();
+const app3 = express();
+const app4 = express();
+const app5 = express();
+const app6 = express();
+const app7 = express();
+const app8 = express();
+const app9 = express();
+const app10 = express();
+
+let instances = [
+  app1,
+  app2,
+  app3,
+  app4,
+  app5,
+  app6,
+  app7,
+  app8,
+  app9,
+  app10
+]
+
+let portCounter = 0;
+instances.forEach(app => {
+  
+let appPort = port + portCounter;
 
 app.use(express.static("./"));
 
-const server = app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+
+
+const server = https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('diufvm08_unifr_ch.pem')
+}, app);
+
+server.listen(port + portCounter, () => console.log('server listening at port ' + appPort));
+
+portCounter++;
 
 const io = new Server(server);
 
@@ -64,4 +104,6 @@ io.on("connection", (client) => {
   });
 
   client.emit("id", client.id);
+});
+
 });
